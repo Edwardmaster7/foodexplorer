784cc97992345f3324cef5024328a6c5039358f0
Edwardmaster7 <eduardoobatista2002@hotmail.com>
2024-08-27 14:32:04 -0300

    Created favourites routes

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -9,6 +9,7 @@ const dishesRouter = require('./dishes.routes');
 const searchRouter = require('./search.routes');
 const categoriesRouter = require("./categories.routes");
 const ordersRouter = require('./orders.routes');
+const favouritesRouter = require('./favourites.routes');
 
 const routes = Router();
 
@@ -19,5 +20,6 @@ routes.use('/dishes', ensureAuthenticated, dishesRouter);
 routes.use('/search', ensureAuthenticated, searchRouter);
 routes.use('/categories', ensureAuthenticated, categoriesRouter);
 routes.use('/orders', ensureAuthenticated, ordersRouter);
+routes.use('/favourites', ensureAuthenticated, favouritesRouter);
 
 module.exports = routes;
