c06c82c29e1e2e9246609cd597573c83e6f3e3c2
Edwardmaster7 <edwardmaster7@hotmail.com>
2024-09-17 18:48:24 -0300

    Small corrections on API's upload config

--- API/src/configs/upload.js
+++ API/src/configs/upload.js
@@ -2,9 +2,22 @@ const path = require('path');
 const multer = require('multer');
 const express = require('express');
 const crypto = require('crypto');
+const fs = require('fs');
+
 
 const TMP_DIR = path.join(__dirname, '..', '..', 'tmp');
+
+// create folder if not exists
+if (!fs.existsSync(TMP_DIR)) {
+  fs.mkdirSync(TMP_DIR);
+}
+
 const UPLOADS_DIR = path.join(TMP_DIR, 'uploads');
+
+// create folder if not exists
+if (!fs.existsSync(UPLOADS_DIR)) {
+  fs.mkdirSync(UPLOADS_DIR);
+}
 
 const MULTER = {
   storage: multer.diskStorage({
