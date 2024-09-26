aa2d08800624f154b2a2b8ed0d208fcd79a1d5f7
Eduardo Oliveira Batista <eduardoobatista2002@hotmail.com>
2024-09-26 12:47:00 -0300

    Mobile search implemented

--- App/foodexplorer-app/src/pages/Menu/index.jsx
+++ App/foodexplorer-app/src/pages/Menu/index.jsx
@@ -1,5 +1,14 @@
-import { useEffect, useState } from "react";
-import { App, Container, Header, SearchWrapper } from "./styles";
+import { useEffect, useState, useMemo } from "react";
+import {
+  App,
+  Container,
+  Header,
+  SearchWrapper,
+  ResultsWrapper,
+  IngredientsWrapper,
+} from "./styles";
+
+import { debounce } from "lodash"; // You'll need to install lodash
 
 import InputField from "../../components/InputField";
 import Footer from "../../components/Footer";
@@ -11,29 +20,77 @@ import { Link } from "react-router-dom";
 
 import { useAuth } from "../../hooks/auth";
 
+import { api } from "../../services/api";
+
 function Menu() {
   const { signOut, user } = useAuth();
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [meals, setMeals] = useState([]);
+
+  // Fetch teh search term results
+  async function loadMeals(searchTerm) {
+    const { data } = await api.get(`/search/${searchTerm}`);
+    setMeals(data.dishes);
+    // console.log(data.dishes);
+  }
+
+  // Memorize the search term, and just call loadMeals 
+  // after 300 ms of the last change
+  const debouncedSearch = useMemo(
+    () => debounce((searchTerm) => loadMeals(searchTerm), 300),
+    []
+  );
+
+  // Handle search input change
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+    debouncedSearch(searchTerm);
+  };
+
+  // Create a memoized version of the ResultsWrapper component,
+  // only re-rendering when the meals array changes
+  const memorizedResultsWrapper = useMemo(() => {
+    if (!meals.length) return null;
+    return (
+       <ResultsWrapper>
+         {meals.map((meal) => (
+           <Link to="/" className="result">
+             <h2>{meal.name}</h2>
+             <p>{meal.description}</p>
+           </Link>
+         ))}
+       </ResultsWrapper>
+     );
+  }, [meals]);
 
   return (
     <App>
       <Header>
         <Link id="close" to="/">
-          <img src={close} alt="close menu"/>
+          <img src={close} alt="close menu" />
         </Link>
         Menu
       </Header>
-
       <Container>
         <SearchWrapper>
-        <InputField
-          icon={search}
-          placeholder="Busque por pratos ou ingredientes"
-        />
-        
+          <InputField
+            icon={search}
+            placeholder="Busque por pratos ou ingredientes"
+            onChange={(e) => handleSearch(e)}
+          />
+          {memorizedResultsWrapper}
         </SearchWrapper>
         <div>
-          {user.isAdmin ? <Link to="/" className="option">Novo Prato</Link> : null}
-          <Link to="/" onClick={signOut} className="option">Sair</Link>
+          {user.isAdmin ? (
+            <Link to="/" className="option">
+              Novo Prato
+            </Link>
+          ) : null}
+          <Link to="/" onClick={signOut} className="option">
+            Sair
+          </Link>
         </div>
       </Container>
     </App>
--- App/foodexplorer-app/src/pages/Menu/styles.js
+++ App/foodexplorer-app/src/pages/Menu/styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const App = styled.div`
   /* background-color: #F5F5F5; */
   display: flex;
@@ -26,7 +25,7 @@ export const Header = styled.header`
   top: 0;
   z-index: 1000;
 
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   font-size: 2.4rem;
 
   img {
@@ -36,7 +35,6 @@ export const Header = styled.header`
   #close:active {
     transform: scale(0.9);
   }
-
 `;
 
 export const Container = styled.div`
@@ -48,7 +46,8 @@ export const Container = styled.div`
   max-width: 1200px;
   height: 100%;
   padding: 3.2rem 2.4rem 1.6rem;
-  
+  min-height: max-content;
+
   gap: 3.2rem;
 
   input {
@@ -59,13 +58,13 @@ export const Container = styled.div`
 
     background-color: ${({ theme }) => theme.colors.dark_900};
   }
-  
+
   .option {
     display: flex;
 
     padding: 0.1rem;
 
-    font-family: 'Poppins', 'sans';
+    font-family: "Poppins", "sans";
     font-size: 2.4rem;
     font-weight: lighter;
     line-height: 140%;
@@ -73,6 +72,78 @@ export const Container = styled.div`
 `;
 
 export const SearchWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const ResultsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* gap: 1rem; */
+  max-height: 50vh;
+  overflow-y: scroll;
+
+  border-radius: 1rem;
+
+  border: 0.3rem solid ${({ theme }) => theme.colors.dark_900};
+
+  &:hover {
+    border: 0.3rem solid ${({ theme }) => theme.colors.deep_green};
+  }
+
+  .result {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.8rem;
+
+    padding: 1.2rem;
+
+    border-bottom: .2rem solid ${({ theme }) => theme.colors.dark_600};
+
+    cursor: pointer;
+
+    transition: all 0.2s ease-in-out;
+
+    background-color: ${({ theme }) => theme.colors.dark_400};
+
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+
+  .result h2 {
+    font-family: "Poppins", sans-serif;
+    font-size: 2rem;
+    font-weight: 500;
+  }
+
+  .result p {
+    font-family: "Roboto", sans-serif;
+    font-size: 1.2rem;
+    line-height: 140%;
+    font-weight: 400;
+
+    color: ${({ theme }) => theme.colors.light_400};
+  }
+
+  .result a {
+    font-family: "Poppins", sans-serif;
+    font-size: 1.6rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.tunrs_green};
+  }
+
+  .result a:active {
+    transform: scale(0.9);
+  }
+`;
+
+export const IngredientsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* gap: 0.4rem; */
 `;
 
 // export const Footer = styled.footer`
