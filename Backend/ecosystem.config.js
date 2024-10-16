b3b0f730a4382a016ddfa2e0833a9492b9539b16
Eduardo Oliveira Batista <eduardoobatista2002@hotmail.com>
2024-10-16 18:49:07 -0300

    modified:   Backend/ecosystem.config.js

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
     instances: "max",
     exec_mode: "cluster",
     env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
       NODE_ENV: "production",
     }
   }]
