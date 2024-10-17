eba5ed07a6de5d0bb58ccd82bd57e2db93abcfb0
Eduardo Oliveira Batista <eduardoobatista2002@hotmail.com>
2024-10-17 09:07:19 -0300

    modified:   Backend/ecosystem.config.js

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
     name: "foodxplorer_api",
     script: "./src/server.js",
     instances: "max",
-    watch: false,
-    exec_mode: "cluster",
     env_production: {
       NODE_ENV: "production",
     }
