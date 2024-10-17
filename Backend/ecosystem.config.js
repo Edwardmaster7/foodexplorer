0e578a8cd13b0fcf83c43f77290726e925b3cea0
Eduardo Oliveira Batista <eduardoobatista2002@hotmail.com>
2024-10-17 10:02:44 -0300

    modified:   Backend/ecosystem.config.js 	modified:   Backend/package.json 	deleted:    Backend/render.yaml

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "foodxplorer_api",
     script: "./src/server.js",
-    instances: 1,
+    instances: max,
     // exec_mode: "fork",
     env_production: {
       NODE_ENV: "production"
